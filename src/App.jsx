17a0a4d9b11a03d731402b24f68b1e189a6773b2
ogagaCodes <ogagaadjaro@gmail.com>
2024-12-20 11:24:53 +0100

    Updates

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./App.css";
+
+const App = () => (
+  <Router>
+    <div className="app">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </div>
+  </Router>
+);
+
+export default App;
